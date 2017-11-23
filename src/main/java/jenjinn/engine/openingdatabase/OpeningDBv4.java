7564cb2fa847aaa143c6adbebbfd7a905f511388
Thomas Ball <Thomas.Ball@lhasalimited.org>
2017-11-23 12:59:20 +0000

    Fixed an issue with io in the opening database

--- src/main/java/jenjinn/engine/openingdatabase/OpeningDBv4.java
+++ src/main/java/jenjinn/engine/openingdatabase/OpeningDBv4.java
@@ -8,9 +8,11 @@ package jenjinn.engine.openingdatabase;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -49,7 +51,11 @@ public class OpeningDBv4
 	{
 		for (final String s : srcpaths)
 		{
-			try (ZipFile zipsrc = new ZipFile(new File(getClass().getResource(s).toURI())))
+			final File temp = File.createTempFile(s, null);
+			final FileOutputStream out = new FileOutputStream(temp);
+			copyStream(getClass().getResourceAsStream(s), out);
+
+			try (ZipFile zipsrc = new ZipFile(temp))
 			{
 				final Enumeration<? extends ZipEntry> entries = zipsrc.entries();
 
@@ -73,8 +79,17 @@ public class OpeningDBv4
 					}
 				}
 			}
+			temp.delete();
 		}
 		return null;
+	}
+
+	public static void copyStream(final InputStream in, final OutputStream out) throws IOException {
+		final byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+		}
 	}
 
 	public ChessMove getMoveForPosition(final BoardState state) throws IOException
